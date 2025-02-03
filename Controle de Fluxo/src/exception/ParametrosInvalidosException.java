2e07f420006988dc4b8a621bf4c94d26af5e149f
Matheus Guedes Bertaiolli <70043048+matheusgbrt@users.noreply.github.com>
2025-02-03 15:55:48 -0300

    feat: Added project UML Iphone

--- Controle de Fluxo/src/exception/ParametrosInvalidosException.java
+++
@@ -1,7 +0,0 @@
-package exception;
-
-public class ParametrosInvalidosException extends Exception {
-    public ParametrosInvalidosException(String message) {
-        super(message);
-    }
-}
---
+++ UML Iphone/src/Iphone.java
@@ -0,0 +1,65 @@
+import Interfaces.*;
+
+class iPhone implements ReprodutorMusical, AparelhoTelefonico, NavegadorInternet {
+
+    @Override
+    public void tocar() {
+        System.out.println("Reproduzindo música...");
+    }
+
+    @Override
+    public void pausar() {
+        System.out.println("Música pausada.");
+    }
+
+    @Override
+    public void selecionarMusica(String musica) {
+        System.out.println("Música selecionada: " + musica);
+    }
+
+    @Override
+    public void ligar(String numero) {
+        System.out.println("Ligando para: " + numero);
+    }
+
+    @Override
+    public void atender() {
+        System.out.println("Chamada atendida.");
+    }
+
+    @Override
+    public void iniciarCorreioVoz() {
+        System.out.println("Iniciando correio de voz...");
+    }
+
+    @Override
+    public void exibirPagina(String url) {
+        System.out.println("Exibindo página: " + url);
+    }
+
+    @Override
+    public void adicionarNovaAba() {
+        System.out.println("Nova aba adicionada.");
+    }
+
+    @Override
+    public void atualizarPagina() {
+        System.out.println("Página atualizada.");
+    }
+
+    public static void main(String[] args) {
+        iPhone meuIphone = new iPhone();
+
+        meuIphone.selecionarMusica("Imagine - John Lennon");
+        meuIphone.tocar();
+        meuIphone.pausar();
+
+        meuIphone.ligar("123-456-789");
+        meuIphone.atender();
+        meuIphone.iniciarCorreioVoz();
+
+        meuIphone.adicionarNovaAba();
+        meuIphone.exibirPagina("https://www.google.com");
+        meuIphone.atualizarPagina();
+    }
+}
